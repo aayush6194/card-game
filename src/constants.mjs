72e2fb5da20896a1cec0e7b1a10aef46ae2c785f
Lawrence Khadka <l.khadka@outlook.com>
2020-10-25 14:42:04 -0600

    actually create numbered cards

--- src/constants.mjs
+++ src/constants.mjs
@@ -20,17 +20,18 @@ export const SUITES_SYMBOLS = {
 export const CARDS = (() => {
     const cardsPoint = { Ace: 15 };
     const facedCards = ['Jack', 'Queen', 'King'];
-    const numberedCards = Array(9).fill(9);
 
     // Assign 10 points to all the faced cards
-    facedCards.map((card) => {
+    facedCards.forEach((card) => {
         cardsPoint[card] = 10;
     });
 
     // Assign 5 points to all the numbered cards cards
-    numberedCards.map((card) => {
-        // offset by 2 on [0...8] because first numbered card is 2 and last is 10.
-        cardsPoint[card + 2] = 5;
-    });
+    Array(9)
+        .fill(0)
+        .forEach((_, i) => {
+            // offset by 2 on [0...8] because first numbered card is 2 and last is 10.
+            cardsPoint[i + 2] = 5;
+        });
     return cardsPoint;
 })();
